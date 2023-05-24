607cfe1433830d4ea0dc975a55c09871f386f568
timosahenru <timosahenru@gmail.com>
2023-05-24 13:39:37 +0000

    rick and morty have universe number

---
+++ main.py
@@ -0,0 +1,9 @@
+class Rick():
+    def __init__(self, universe):
+        self.universe = universe
+        
+        
+        
+class Morty():
+    def __init__(self, universe):
+        self.universe = universe
