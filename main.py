499dc0a40f100b8bafc32e2647c98150c2c4484b
timosahenru <timosahenru@gmail.com>
2023-05-24 07:14:44 +0000

    morty universe created

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 class Rick():
     def __init__(self, universe):
         self.universe = universe
+        
+        
+        
+class Morty():
+    def __init__(self, universe):
+        self.universe = universe
+        
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from main import Rick
+from main import Rick, Morty
 
 
 class TestRick(unittest.TestCase):
@@ -7,6 +7,14 @@ class TestRick(unittest.TestCase):
     def test_universe_number(self):
         rick = Rick(1)
         self.assertEqual(rick.universe, 1)
+        
+        
+        
+class TestMorty(unittest.TestCase):
+    
+    def test_universe_number(self):
+        morty = Morty(2)
+        self.assertEqual(morty.universe, 2)
 
 
 if __name__ == "__main__":
