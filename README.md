0b8f56cca8bb97dd65d89503a3c0bece826e9cbf
timosahenru <timosahenru@gmail.com>
2023-05-24 13:56:52 +0000

    residents can be added to citadel

--- README.md
+++ README.md
@@ -5,6 +5,9 @@
 ## Users story
 - ### A user is able to assign Ricks and Mortys a universe number
 - ### A user is able to add residents to the Citadel
+    - #### Citadel has a residents list
+    - #### Citadel can get all residents
+    - #### Citadel can add residents
 - ### A user is able to turn all Ricks with assigned Mortys to pickles
     - #### Rick has a morty
     - #### Rick has a pickle
--- main.py
+++ main.py
@@ -7,3 +7,18 @@ class Rick():
 class Morty():
     def __init__(self, universe):
         self.universe = universe
+        
+        
+        
+class Citadel():
+    def __init__(self):
+        self.residents = []
+        
+        
+        
+    def get_all_residents(self):
+        return self.residents
+    
+    
+    def add_residents(self, resident):
+        self.residents.append(resident)
